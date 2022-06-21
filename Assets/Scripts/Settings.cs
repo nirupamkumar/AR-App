57819cfe1699c09ab1b16494edf9a36d01652c48
nirupamkumar <mundra.nirupamkumar@gmail.com>
2022-06-21 06:49:25 +0200

    few changes

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization;
+
+public class Settings : MonoBehaviour
+{
+    public Toggle toggle;
+
+    public void ToggleSwitch()
+    {
+        if(!toggle.isOn)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
